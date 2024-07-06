64dfa2ada6d1ad16e949e0b9426ca51795731c9c
abocca <abocca@ext.sancorseguros.com>
2024-07-05 23:36:11 -0300

    Arreglos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,11 @@ module.exports = {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans: ['Monserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
