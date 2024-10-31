0cbd62f1adb2c3eb165155f1c0c76eb1d50eeff5
abocca <abocca@ext.sancorseguros.com>
2024-10-31 00:52:12 -0300

    Se agregan cambios y se traduce web

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,14 @@ export default defineConfig({
   integrations: [tailwind()],
   server: {
     port: 4001,
-    https: {
-      key: fs.readFileSync('./https/certificate.key'),
-      cert: fs.readFileSync('./https/certificate.pem')
-    }
+    // https: {
+    //   key: fs.readFileSync('./https/certificate.key'),
+    //   cert: fs.readFileSync('./https/certificate.pem')
+    // }
+  },
+  i18n:{
+    defaultLocale: 'es',
+    locales:['es' , 'en'],
+    routing: { prefixDefaultLocale : 'false'}
   }
 })
---
+++ src/i18n/ui.ts
@@ -0,0 +1,32 @@
+export const languages = {
+    es: 'Español',
+    en: 'English'
+  };
+  
+  export const showDefaultLang = false;
+  export const defaultLang = 'es';
+  
+  export const ui = {
+    es: {
+        'nav.home': 'Inicio',
+        'nav.pilares': 'Pilares',
+        'nav.staff': 'Staff',
+        'nav.ProductosDestacados': 'Productos Destacados',
+        'nav.Colecciones': 'Colecciones', 
+        'nav.Coleccion2023': 'Colección 2023',
+        'nav.Coleccion2024': 'Colección 2024',
+        'nav.YeguasMadre': 'Yeguas Madre',
+        'nav.Contacto': 'Contacto'
+      },
+    en: {
+      'nav.home': 'Home',
+        'nav.pilares': 'Pillars',
+        'nav.staff': 'Staff',
+        'nav.ProductosDestacados': 'Featured Products',
+        'nav.Colecciones': 'Collections',
+        'nav.Coleccion2023': 'Collection 2023',
+        'nav.Coleccion2024': 'Collection 2024',
+        'nav.YeguasMadre': 'Yeguas Madre',
+        'nav.Contacto': 'Contact'
+    }
+  } as const;
